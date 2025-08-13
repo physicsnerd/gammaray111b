777f3ab20b6f0a8ed933b4c0052b1342644f40db
Auden Young <auden.nerd@gmail.com>
2025-08-13 14:42:24 -0700

    documented ADS file, moved over lab 10 code more fully

--- GMAMicroscope/ScopeFoundryHW/ADS.py
+++ GMAMicroscope/ScopeFoundryHW/ADS.py
@@ -2,54 +2,85 @@ from ScopeFoundry import HardwareComponent
 from WF_SDK import device
 from WF_SDK import scope
 from WF_SDK import wavegen
-from WF_SDK import supplies
-from WF_SDK import dmm
-from WF_SDK import logic
-from WF_SDK import pattern
-from WF_SDK import static
-from WF_SDK import protocol
-from WF_SDK.protocol import i2c
-from WF_SDK.protocol import spi
-from WF_SDK.protocol import uart
 
 class ADSHardware(HardwareComponent):
+    """Class of functions for interfacing with the ADS.
+    """
 
     def setup(self):
         self.name = 'ads'
-        #self.settings.New('channel', dtype=str, initial='/Dev2/ao0')
-        self.handle = device.open()
-    
+        self.handle = None
+
     def connect(self):
-        # this is the device handle - it will be used by all functions to "address" the connected device
+        """Connects to the ADS. Defines 'handle', the address to the ADS.
+        Must be run at the beginning of every program using the ADS.
+        """
         self.handle = device.open()
-    
-    def open_scope(self, buffer_size=1000, sample_freq=100e6):
+
+    def open_scope(self, buffer_size=1000, sample_freq=1e6):
+        """Opens connection to the scope.
+
+        Args:
+            buffer_size (int, optional): How many data points are temporarily stored
+            before being returned. The buffer is a temporary slot for storing a small amount of
+            data before it is transferred to its final destination. Defaults to 1000.
+            sample_freq (int, optional): How frequently the oscilloscope will sample
+            from the input. Defaults to 1e6. You can decrease this if you have too
+            many data points/the function is taking awhile to run for the time scale you need.
+            (16e3 is a reasonable selection.)
+        """
+        self.buffer_size = buffer_size
         scope.open(self.handle, buffer_size=buffer_size, sampling_frequency=sample_freq)
-        #buffer = scope.record(self.handle, channel=channel)
-        #time.sleep(wait)
-        #scope.close(self.handle)
-        #return buffer
-        return
-    
+
     def trigger_scope(self, channel=1, level=0.1):
-        scope.trigger(self.handle, enable=True, source=scope.trigger_source.analog, channel=channel, edge_rising=True, level=level)
-        return
-    
+        """Sets trigger level for the scope. Kind of a buggy function; not used.
+
+        Args:
+            channel (int, optional): Selects which channel of scope to read out. 
+            Defaults to 1.
+            level (float, optional): Sets trigger level for scope. Defaults to 0.1.
+        """
+        scope.trigger(self.handle, enable=True, source=scope.trigger_source.analog, channel=channel,
+                      edge_rising=True, level=level)
+
     def read_scope(self, channel=1):
+        """Collects data from the scope.
+
+        Args:
+            channel (int, optional): Which channel to read from. Defaults to 1.
+
+        Returns:
+            buffer (array): An array of output data points. The buffer is a temporary slot 
+            for storing a small amount of data before it is transferred to its final destination.
+        """
         buffer = scope.record(self.handle, channel=channel)
         return buffer
-    
+
     def close_scope(self):
+        """Closes connection to the scope.
+        """
         scope.close(self.handle)
-        return
-    
-    def use_wavegen(self, channel=1, function=wavegen.function.sine, offset=0, freq=10e3, amp=1):
-        wavegen.generate(self.handle, channel=channel, function=function, offset=offset, frequency=freq, amplitude=amp)
-        return
+
+    def use_wavegen(self, channel=1, function=wavegen.function.sine, offset_v=0, freq_hz=1e3, amp_v=1):
+        """Runs the wavegen producing function with given parameters.
+
+        Args:
+            channel (int, optional): Which channel output is at. Defaults to 1.
+            function (function object, optional): What type of function to output. 
+            Defaults to wavegen.function.sine.
+            offset (int, optional): Voltage offset (V). Defaults to 0.
+            freq (int, optional): Frequency (Hz). Defaults to 1e3.
+            amp (int, optional): Amplitude (V). Defaults to 1.
+        """
+        wavegen.generate(self.handle, channel=channel, function=function, offset=offset_v,
+                         frequency=freq_hz, amplitude=amp_v)
 
     def close_wavegen(self):
+        """Closes wavegen.
+        """
         wavegen.close(self.handle)
-        return
-   
+
     def disconnect(self):
+        """Closes ADS connection. Must be run at the end of every program.
+        """
         device.close(self.handle)
