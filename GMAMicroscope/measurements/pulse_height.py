reverted test - it is reading from channel 1, just the completely wrong values somehow

GMAMicroscope/measurements/pulse_height.py
@@ -49,7 +49,7 @@ class PulseHeightAnalyze(Measurement):
 
         while legit_data_points <= self.settings["N"]:
             data_points += 1
-            buffer = np.array(hw.read_scope(channel=2))
+            buffer = np.array(hw.read_scope())
             print(np.max(buffer))
 
             # measure deadtime